fb8e198f16db2fa7b1a4a3a45f45c28d7b4f041e
3961k <80889322+3961K@users.noreply.github.com>
2021-08-25 13:34:56 +0000

    add カードの一覧取得と新規作成を行うためのAPIViewとシリアライザを作成した
    一覧取得時に利用するCardSerializerを作成し,ユニットテストによる検証を行った
    新規作成時に利用するCardCreateSerializerを作成し,ユニットテストによる検証を行った
    一覧取得と新規作成を行うCardListCreateAPIViewを作成し,ユニットテストによる検証を行った

---
+++ api/v1/cards/tests/test_card_serializer.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+
+from ..models import Card
+from ..serializers import CardSerializer
+
+User = get_user_model()
+
+
+class CardSerializerTest(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user = User.objects.create(
+            username='card_serializer',
+            email='card_serializer@test.com',
+            password='testpassw0rd123',
+        )
+        user.save()
+        for i in range(1, 6):
+            Card.objects.create(
+                word='card_serializer{}'.format(i),
+                answer='card_serializer{}'.format(i),
+                author=user
+            )
+
+    def test_1_success_get_list(self):
+        cards = Card.objects.all()
+        serializer = CardSerializer(instance=cards, many=True)
+        # 予想した件数を取得する事が出来る
+        self.assertEqual(len(serializer.data), 5)
