ffc894ab5cc285fa38c5b368e78dd29e4946061d
3961k <80889322+3961K@users.noreply.github.com>
2021-09-04 15:35:15 +0000

    add ユーザ検索を行うWebページのためのUsersView, html, css, jsを作成した
    - UserListView, users.html, users.css, users.jsを作成した

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('authentication/', include('authentication_front.urls'))
+    path('authentication/', include('authentication_front.urls')),
+    path('users/', include('users_front.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
