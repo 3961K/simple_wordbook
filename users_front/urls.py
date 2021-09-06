cc34a21631e028c0f9db90c4c70935091f08beaf
3961k <80889322+3961K@users.noreply.github.com>
2021-09-06 10:45:12 +0000

    add ユーザが作成したカード一覧を表示するWebページのためのUserCardsView, html, css, jsを作成した

--- users_front/urls.py
+++ users_front/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import UsersView
+from .views import UsersView, UserCardsView
 
 app_name = 'users_front'
 
 urlpatterns = [
     path('', UsersView.as_view(), name='list'),
+    path('<str:username>/', UserCardsView.as_view(), name='top'),
+    path('<str:username>/cards/', UserCardsView.as_view(), name='cards'),
 ]
--- users_front/views.py
+++ users_front/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class UsersView(TemplateView):
     template_name = 'users_front/users.html'
+
+
+class UserCardsView(TemplateView):
+    template_name = 'users_front/user_cards.html'
