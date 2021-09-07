cfb38f29cdd5cadb74a786e8fbb6115876eb6a85
3961k <80889322+3961K@users.noreply.github.com>
2021-09-07 06:56:30 +0000

    add カードの一覧表示・検索を行うWebページのためのCardsView, html, css, jsを追加した
    - CardsView, cards,html, cards.css, cards.jsを追加した

--- cards_front/views.py
+++ cards_front/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class CardsView(TemplateView):
+    template_name = 'cards_front/cards.html'
