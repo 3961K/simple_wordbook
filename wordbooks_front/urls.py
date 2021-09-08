ef2a9138e31b449ede59c5bf045dcde71245c7b6
3961k <80889322+3961K@users.noreply.github.com>
2021-09-08 19:26:16 +0000

    add 単語帳の詳細WebページのためのWordbookView, html, css, jsを追加
    - WordbookView, wordbook.html, wordbook.css, wordbook.jsを追加

--- wordbooks_front/urls.py
+++ wordbooks_front/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import WordbooksView
+from .views import WordbooksView, WordbookView
 
 app_name = 'wordbooks_front'
 
 urlpatterns = [
     path('', WordbooksView.as_view(), name='list'),
+    path('<uuid:id>/', WordbookView.as_view(), name='detail'),
 ]
